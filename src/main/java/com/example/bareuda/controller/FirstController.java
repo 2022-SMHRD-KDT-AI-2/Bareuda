ce15aef4c903bb10ae52b18e67f6266883843fe2
soyeon <qkrthdus7077@naver.com>
2022-05-20 11:52:09 +0900

    test

--- src/main/java/com/example/bareuda/controller/FirstController.java
+++ src/main/java/com/example/bareuda/controller/FirstController.java
@@ -9,7 +9,7 @@ public class FirstController {
 
     @GetMapping("/hi")
     public String niceToMeetYou(Model model) {
-        return "baumannTest4";
+        return "index";
     }
 
     @GetMapping("/bye")
