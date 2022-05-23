bc0be86078884ee7167ac5d879c13eccb50a20f8
LEE-DO-YEON <gcdalee@gmail.com>
2022-05-23 15:33:38 +0900

    성분 출력 추가

---
+++ src/main/java/com/example/bareuda/entity/Ingredients.java
@@ -0,0 +1,14 @@
+package com.example.bareuda.entity;
+
+import lombok.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class Ingredients {
+    private int i_id;
+    private String i_name;
+    private String i_result;
+}
