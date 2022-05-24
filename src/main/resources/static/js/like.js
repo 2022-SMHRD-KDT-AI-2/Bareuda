b9e2016aadcbfb4a1ce9189d615fcf8792e63c01
soyeon <qkrthdus7077@naver.com>
2022-05-24 12:32:36 +0900

    productDetail.mustache

--- src/main/resources/static/js/like.js
+++ src/main/resources/static/js/like.js
@@ -10,21 +10,30 @@ function generateClones(id){
 }
 */
 
+//function generateClones(id){
+//    $.ajax({
+//    url: "/product/like.do",
+//    type: "POST",
+//    cache: false,
+//    dataType: "json",
+//    data: {"p_id":id},
+//    success:
+//    function(data){
+//        //alert("like 성공");
+//        $("#p"+data.p_id).toggleClass('liked');
+//    },
+//    error:
+//    function (request, status, error){
+//    alert("ajax실패. code:"+request.status+"\n"+"message:"+request.reponseText+"\n"+"error:"+error);
+//    }
+//    });
+//}
 function generateClones(id){
-    $.ajax({
-    url: "/product/like.do",
-    type: "POST",
-    cache: false,
-    dataType: "json",
-    data: {"p_id":id},
-    success:
-    function(data){
-        //alert("like 성공");
-        $("#p"+data.p_id).toggleClass('liked');
-    },
-    error:
-    function (request, status, error){
-    alert("ajax실패. code:"+request.status+"\n"+"message:"+request.reponseText+"\n"+"error:"+error);
+    let button = document.querySelector("#p"+id);
+    //alert($("#p"+id));
+    if (button.classList.contains("liked")) {
+        button.classList.remove("liked");
+    } else {
+        button.classList.add("liked");
     }
-    });
 }
