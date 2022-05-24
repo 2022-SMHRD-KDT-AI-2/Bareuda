91d037d7235159dba2dafc4686c6eafaf2802445
soyeon <qkrthdus7077@naver.com>
2022-05-24 17:29:54 +0900

    이미지추가

--- src/main/resources/static/js/doughnutChart.js
+++
@@ -1,7 +0,0 @@
-
-
-var myChart2 = drawDoughnut(['건성에 좋아요','복합성에 좋아요','지성에 좋아요'],[20,30,50], 'myChart2');
-var myChart3 = drawDoughnut(['보습에 좋아요','진정에 좋아요','주름/미백에 좋아요'],[50,20,30], 'myChart3');
-var myChart4 = drawDoughnut(['자극없이 순해요','보통이에요','자극이 느껴져요'],[30,20,50], 'myChart4');
-
-
