b05a64a80043b1f47b98d77ccafe3706c0071aaf
soyeon <qkrthdus7077@naver.com>
2022-05-25 12:08:49 +0900

    word

--- src/main/resources/static/js/cloud.js
+++ src/main/resources/static/js/cloud.js
@@ -1,24 +1,28 @@
-anychart.onDocumentReady(function() {
-    var data = [
-      {"x": "Mandarin chinese", "value": 200},
-      {"x": "English", "value": 100},
-      {"x": "Hindustani", "value": 300},
-    ];
+function drawWord(keylist,valuelist,chartName){
+    anychart.onDocumentReady(function() {
+        
+        var data = [
+            [keylist[0], valuelist[0]],
+            [keylist[1], valuelist[1]],
+            [keylist[2], valuelist[2]]
+          ];
+      
+       // create a tag (word) cloud chart
+        var chart = anychart.tagCloud(data);
+        
+        chart.title('긍정 워드클라우드')
+        // set an array of angles at which the words will be laid out
+        chart.angles([0])
+        // enable a color range
+        chart.colorRange(true);
+        // set the color range length
+        chart.colorRange().length('80%');
+      
+        // display the word cloud chart
+        chart.container(chartName);
+        chart.draw();
+      });
+}
 
-   // create a tag (word) cloud chart
-    var chart = anychart.tagCloud(data);
-
-     // set a chart title
-    chart.title('긍정 워드클라우드')
-    // set an array of angles at which the words will be laid out
-    chart.angles([0])
-    // enable a color range
-    chart.colorRange(true);
-    // set the color range length
-    chart.colorRange().length('80%');
-
-    // display the word cloud chart
-    chart.container("container");
-    chart.draw();
-
-  });
+var myChart8 = drawWord(['Hello', 'world', 'normally'],[90, 70, 60],'myChart8' );
+var myChart9 = drawWord(['Hello', 'world', 'normally'],[90, 70, 60],'myChart9' );
