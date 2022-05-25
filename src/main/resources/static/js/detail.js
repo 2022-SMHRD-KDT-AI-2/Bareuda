7b3d46aba4ff3aa75a991c80da21a868a41ff5d3
soyeon <qkrthdus7077@naver.com>
2022-05-25 09:24:57 +0900

    pie, horizontal, word

--- src/main/resources/static/js/detail.js
+++ src/main/resources/static/js/detail.js
@@ -124,3 +124,147 @@ function drawRadar(datalist, chartName) {
     })
     return myChartR;
 };
+
+/* 차트 추가 (pie, horizontal, word ) */
+function drawRadar(reviewlist, chartName) {
+
+    var context = document
+        .getElementById(chartName)
+        .getContext('2d');
+    var myChartP = new Chart(context, {
+        type: 'pie',
+        elements:{
+            point:{
+                radius:0
+            },
+        },
+        data: { // 차트에 들어갈 데이터
+            labels: [
+                //x 축
+                '긍정율','부정율'
+            ],
+            datasets: [
+                { //데이터
+                    label: '분포도', //차트 제목
+                    fill: true, // line 형태일 때, 선 안쪽을 채우는지 안채우는지
+                    data: reviewlist,
+                    backgroundColor: [
+                        'rgba(	116, 187, 235, 0.5)',
+                    ],
+                    borderColor: [
+                        'rgba(	116, 187, 235, 1)',
+                    ],
+                    borderWidth: 5
+                }
+            ]
+        },
+        options: {
+            scale:{
+                ticks:{
+                    beginAtZero:true,
+                    display: false,
+                    min:0,
+                    max:50,
+                    stepSize:10
+                }
+            }
+        }
+    })
+    return myChartP;
+};
+
+
+
+var myChart5 = drawRadar([80,20], 'myChart5');
+
+
+/* horizontal */
+
+function drawHorizontal(keywordlist, datalist, chartName) {
+    var context = document
+        .getElementById(chartName)
+        .getContext('2d');
+    var myChartH = new Chart(context, {
+        type: 'horizontalBar',
+        elements:{
+            point:{
+                radius:0
+            },
+        },
+        data: { // 차트에 들어갈 데이터
+            labels: [
+                //x 축
+                keywordlist[0],keywordlist[1],keywordlist[2],keywordlist[3],keywordlist[4],
+                keywordlist[5],keywordlist[6],keywordlist[7],keywordlist[8],keywordlist[9]
+            ],
+            datasets: [
+                { //데이터
+
+                    label: '빈도수', //차트 제목
+                    fill: true, // line 형태일 때, 선 안쪽을 채우는지 안채우는지
+                    data: datalist,
+                    backgroundColor: [
+                        'rgba(	116, 187, 235, 0.5)',
+                    ],
+                    borderColor: [
+                        'rgba(	116, 187, 235, 1)',
+                    ],
+                    borderWidth: 5,
+                }
+            ]
+        },
+        options: {
+
+            scale:{
+                ticks:{
+                    beginAtZero:true,
+                    display: false,
+                    min:0,
+                    max:50,
+                    stepSize:10
+                }
+            }
+        }
+    })
+
+    return myChartH;
+};
+
+var myChart6 = drawHorizontal(['가','나','다','라','마','바','사','아','자','차'],[10,9,8,7,6,5,4,3,2,1], 'myChart6');
+var myChart7 = drawHorizontal(['가','나','다','라','마','바','사','아','자','차'],[10,9,8,7,6,5,4,3,2,1], 'myChart7');
+
+
+/* word */
+
+function drawWord(keylist, valuelist, chartName) {
+    var context = document.getElementById(chartName).getContext('2d');
+    var myChartW = new Chart(context, {
+        type: 'wordCloud',
+        data: {
+            labels: [
+              keylist[0],keylist[1],keylist[2]
+            ],
+            datasets: [
+                {
+                label: '',
+                data: valuelist,
+                },
+            ],
+        },
+        options: {
+            title: {
+              display: false,
+              text: "Chart.js Word Cloud"
+            },
+            plugins: {
+              legend: {
+                display: false
+              }
+            }
+
+}})
+        return myChartW;
+  };
+
+  var myChart8 = drawWord(['Hello', 'world', 'normally'],[90, 70, 60],'myChart8' );
+  var myChart9 = drawWord(['Hello', 'world', 'normally'],[90, 70, 60],'myChart9' );
