ddaecf4788289e12d1093ebecfa489fec3bdfc93
soyeon <qkrthdus7077@naver.com>
2022-05-26 17:51:41 +0900

    d

--- src/main/resources/static/js/detail.js
+++ src/main/resources/static/js/detail.js
@@ -309,7 +309,7 @@ function drawWord(keylist,valuelist,chartName){
         // display the word cloud chart
         chart.container(chartName);
         chart.normal().fill("#609efa");
-        chart.getCredits().setEnabled(false);
+
         chart.draw();
       });
 }
