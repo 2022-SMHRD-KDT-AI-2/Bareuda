7bf584fa7495dbd184b04738a876ceba7116e4af
LEE-DO-YEON <gcdalee@gmail.com>
2022-05-27 14:40:40 +0900

    결과페이지 그림이랑 타입 출력 부분 수정.

--- src/main/java/com/example/bareuda/controller/BaumannController.java
+++ src/main/java/com/example/bareuda/controller/BaumannController.java
@@ -30,6 +30,11 @@ public class BaumannController {
         model.addAttribute("answer", answer);
         Ingredients ingredients = baumannService.getRecommendedIngredients(answer.getMb_result());
         model.addAttribute("Ingredients", ingredients.getI_name().replace("<br>", "\n"));
+
+        model.addAttribute("type1", answer.getMb_result().charAt(0));
+        model.addAttribute("type2", answer.getMb_result().charAt(1));
+        model.addAttribute("type3", answer.getMb_result().charAt(2));
+        model.addAttribute("type4", answer.getMb_result().charAt(3));
         return "baumannResult";
     }
 
